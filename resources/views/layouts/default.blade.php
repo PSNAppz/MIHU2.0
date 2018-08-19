d58e4307716af463816a72a7e9267d50ddd4abbf
PS NaraYanan <thepsnarayanan@gmail.com>
2018-08-19 23:31:01 +0530

    UI/UX

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,19 +12,22 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet" type="text/css">
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body id="body_color">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" id="nav_color">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/') }}" style="color:white;">
                     MIHU 2.0 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -44,12 +47,12 @@
 
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="color:white;">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a class="dropdown-item" href="{{ route('logout') }}" style="color:white;"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ ('Logout') }}
@@ -70,5 +73,58 @@
             @yield('content')
         </main>
     </div>
+
+    <style type="text/css">
+
+        #card_gradient1{
+            background: #8E2DE2;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to left, #4A00E0, #8E2DE2);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to left, #4A00E0, #8E2DE2); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        }
+        #card_gradient2{
+            background: #F7971E;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to left, #FFD200, #F7971E);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to left, #FFD200, #F7971E); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+        }
+        #card_gradient3{
+            background: #C04848;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to left, #480048, #C04848);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to left, #480048, #C04848); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+        }
+        #card_gradient4{
+            background: #ee0979;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to left, #ff6a00, #ee0979);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to left, #ff6a00, #ee0979); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+        }
+        #card_gradient5{
+            background: #ff00cc;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to left, #333399, #ff00cc);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to left, #333399, #ff00cc); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+        }
+        #card_gradient6{
+            background: #00C9FF;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to left, #92FE9D, #00C9FF);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to left, #92FE9D, #00C9FF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+        }
+        #body_color{
+            background: #0f0c29;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+        }
+        #nav_color{
+            background: #0f0c29;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #24243e, #040138, #040138);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #24243e, #040138, #040138); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            color:white;
+        }
+
+    </style>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,156 @@
-@extends('layouts.default')
+@extends('layouts.default') @section('content')
+<!-- IMAGE View
+<div class="container " id="#body_color">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="{{ asset('images/backcover.jpg') }}" alt="First slide">
+            </div>
+        </div>
+    </div>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    --> 
+    <br> 
+    @if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+    @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container ">
+        <!-- Row -->
+        <div class="row">
+            <div class="col-sm">
+                <div class="card" id="card_gradient1" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Accommodation</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">View Accommodation details</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
                         </div>
-                    @endif
+                    </div>
+                </div>
+            </div>
 
-                    MIHU 2.0
+            <div class="col-sm">
+                <div class="card" id="card_gradient1" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Darshan</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">Darshan Timings</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card" id="card_gradient1" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Food</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">Food Counters</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card" id="card_gradient4" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Events</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">Special Events and Timings</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card" id="card_gradient4" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Transportation</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">View transportation details</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card" id="card_gradient4" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Volunteers</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">Volunteers of Amritavarsham 65</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-sm">
+                <div class="card" id="card_gradient2" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">VCC</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">View VCC Timings</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card" id="card_gradient2" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Emergency</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">Emergency contact details</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"> Click Here
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm">
+                <div class="card" id="card_gradient2" style="width: 18rem;text-align: center;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">VIP Details</h5>
+                        <h6 class="card-subtitle mb-2 text" style="color: white;">VIP details</h6>
+                        <div class="btn-group-toggle" data-toggle="buttons">
+                            <label class="btn btn-dark active">
+                                <input type="checkbox" checked autocomplete="off"/> Click Here
+                            </label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
