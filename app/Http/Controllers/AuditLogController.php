9ee306321730656f8d22341556865fb3e4fedc53
PS NaraYanan <thepsnarayanan@gmail.com>
2018-08-21 12:39:19 +0530

    Update

---
+++ app/Http/Controllers/AuditLogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AuditLog;
+use Illuminate\Http\Request;
+
+class AuditLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AuditLog  $auditLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AuditLog $auditLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AuditLog  $auditLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AuditLog $auditLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AuditLog  $auditLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AuditLog $auditLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AuditLog  $auditLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AuditLog $auditLog)
+    {
+        //
+    }
+}
