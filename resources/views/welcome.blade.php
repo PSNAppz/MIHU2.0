16a0928917ceb7aa6bf56cc8b33d70669c4478f8
amitk936 <amitkrishna936@gmail.com>
2018-08-21 20:22:39 +0530

    Hyperlink, decoraton

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,35 +26,39 @@
         <div class="row">
             <div class="col-sm">
                 <div class="card" id="card_gradient1" style="width: 18rem;text-align: center;">
-                   
+                    <a href="#" style="text-decoration:none;">    
                         <div class="card-body">
+                        
                             <i style="font-size:40px;color:white;" class="fa fa-building icon"></i>
                             <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Accommodation</h5>
                             <h6 class="card-subtitle mb-2 text" style="color: white;">View Accommodation details</h6>
                        
-                    </div>
+                        </div>
+                    </a>   
                 </div>
                 
             </div>
 
             <div class="col-sm">
                 <div class="card" id="card_gradient1" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                    <a href="#" style="text-decoration:none;"><div class="card-body">
                    
                     <i style="font-size:40px;color:white" class="fa fa-users icon"></i>
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Darshan</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">Darshan Timings</h6>
                     </div>
+                    </a>
                 </div>
             </div>
 
             <div class="col-sm">
                 <div class="card" id="card_gradient1" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                   <a href="#" style="text-decoration:none;"> <div class="card-body">
                     <i style="font-size:40px;color:white;" class="fa fa-cutlery"></i>
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Food</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">Food Counters</h6>
                     </div>
+                   </a>
                 </div>
             </div>
         </div>
@@ -62,31 +66,34 @@
         <div class="row">
             <div class="col-sm">
                 <div class="card" id="card_gradient4" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                    <a href="#" style="text-decoration:none;"><div class="card-body">
                         <i style="font-size:40px;color:white;" class="fa fa-calendar"></i>
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Events</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">Special Events and Timings</h6>
                     </div>
+                    </a>
                 </div>
             </div>
 
             <div class="col-sm">
                 <div class="card" id="card_gradient4" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                    <a href="#" style="text-decoration:none;"><div class="card-body">
                     <i class="fa fa-plane icon" style="font-size:40px;color:white"></i>
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Transportation</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">View transportation details</h6>
                     </div>
+                    </a>
                 </div>
             </div>
 
             <div class="col-sm">
                 <div class="card" id="card_gradient4" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                    <a href="#" style="text-decoration:none;"><div class="card-body">
                     <i class="fa fa-user-plus" style="font-size:40px;color:white"></i>
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Volunteers</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">Volunteers of Amritavarsham 65</h6>
                     </div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -94,31 +101,34 @@
         <div class="row">
             <div class="col-sm">
                 <div class="card" id="card_gradient2" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                   <a href="#" style="text-decoration:none;"> <div class="card-body">
                     <i class="fa fa-handshake-o icon" style="font-size:40px;color:white"></i>
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">VCC</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">View VCC Timings</h6>
                     </div>
+                   </a>
                 </div>
             </div>
 
             <div class="col-sm">
                 <div class="card" id="card_gradient2" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                   <a href="#" style="text-decoration:none;"> <div class="card-body">
                     <i class="fa fa-phone" style="font-size:40px;color:white"></i> 
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">Emergency</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">Emergency contact details</h6>
                     </div>
+                   </a>
                 </div>
             </div>
 
             <div class="col-sm">
                 <div class="card" id="card_gradient2" style="width: 18rem;text-align: center;">
-                    <div class="card-body">
+                    <a href="#" style="text-decoration:none;"><div class="card-body">
                     <i class="fa fa-user-circle"  style="font-size:40px;color:white"></i>
                         <h5 class="card-title" style="font-weight: bold;color: white;font-family: 'Fugaz One', cursive;font-size: 25px">VIP Details</h5>
                         <h6 class="card-subtitle mb-2 text" style="color: white;">VIP details</h6>
                     </div>
+                    </a>
                 </div>
             </div>
         </div>
