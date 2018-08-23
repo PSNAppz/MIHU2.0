d5d934451e6ab30249f42aad1c8b6af32349e4a0
amitk936 <amitkrishna936@gmail.com>
2018-08-24 00:02:38 +0530

    Media Page Table Mod

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -1,62 +1,72 @@
 @extends('layouts.default') @section('content')
 <!--Update Images with the Logos of each channels-->
 <!--Buttons for Link of Channels website-->
-<h3 style="color:aliceblue;text-align:center">Media Covering Live Telecast at Amritapuri</h3>
-    <div class="row">
+<style>
+    li{
+    padding: 1em;
+    text-align: center;
+    font-size: 17pt;
+    font-family: 'Dosis', Arial, sans-serif;  
+    }
+</style>
+<h3 style="color:aliceblue;text-align:center"><b>Media Covering Live Telecast at Amritapuri</b></h3>
+    <center>
+        <div class="row">
         <div class="col-sm-4 col-md-4">
-            <div class="pricingTable purple">
-                <div class="pricingTable-header">
+            <div class="p-3 mb-2 bg-light text-dark">
+                <div class="p-3 mb-2 bg-light text-dark">
                     <img class="rounded-circle" src="{{ asset('images/2.jpg') }}" style="height:110px;width:110px">
-                    <h3>ASIAnet news</h3><span>Asianet News Kochin Desk</span></div>
-                <div class="pricing-plans"><b><span class="priceContent" style="text-size:90px;color:floralwhite"><span>Crew Members</span></span></b></div>
-                <div class="pricingContent" style="background:azure">
-                    <ul>
-                        <li>Jaya</li>
-                        <li>Anand</li>
-                        <li>James</li>
-                        <li>Ram</li>
-                    </ul>
+                    <b><h3>Asianet news</h3></b><span><b>Asianet News Kochin Desk</b></span></div>
+                <div class="p-3 mb-2 bg-light text-dark"><b><span class="priceContent" style="text-size:90px;color:black"><span><h5>Crew Members</h5></span></span></b></div>
+                <div class="p-3 mb-2 bg-light text-dark" style="background:azure">
+                <div class="p-3 mb-2 bg-light text-dark">
+                    Jaya<br/>
+                    Anand<br/>
+                    James<br/>      
+                    Ram<br/>
                 </div>
-                <div class="pricingTable-sign-up"><a href="#" class="btn btn-block">Website</a></div>
+                </div>
+                <b style="color:gold"><div class="btn btn-danger"><a href="#" class="btn btn-block"><b>Website</b></a></div></b>
             </div>
         </div>
         <div class="col-sm-4 col-md-4">
-            <div class="pricingTable green">
-                <div class="pricingTable-header">
+            <div class="p-3 mb-2 bg-light text-dark">
+                <div class="p-3 mb-2 bg-light text-dark">
                     <img class="rounded-circle" src="{{ asset('images/2.jpg') }}" style="height:110px;width:110px">
-                    <h3>amrita tv</h3><span>Amrita TV Trivandrum Desk</span></div>
-                <div class="pricing-plans"><b><span class="priceContent" style="text-size:90px;color:antiquewhite"><span>Crew Members</span></span></b>
+                    <b><h3>Amrita tv</h3><span>Amrita TV Trivandrum Desk</span></b></div>
+                <h5><div class="p-3 mb-2 bg-light text-dark"><b><span class="priceContent" style="text-size:90px;color:navy"><span>Crew Members</span></span></b>
+                </div></h5>
+                <div class="p-3 mb-2 bg-light text-dark" style="background:azure">
+                    <div class="p-3 mb-2 bg-light text-dark">
+                        Lakshmi<br/>
+                        Raji<br/>
+                        Arjun<br/>
+                        Jayashree<br/>
+                    </div class="p-3 mb-2 bg-light text-dark">
                 </div>
-                <div class="pricingContent" style="background:azure">
-                    <ul>
-                        <li>Lakshmi</li>
-                        <li>Raji</li>
-                        <li>Arjun</li>
-                        <li>Jayashree</li>
-                    </ul>
-                </div>
-                <div class="pricingTable-sign-up"><a href="#" class="btn btn-block">website</a></div>
+                <div class="btn btn-danger"><a href="#" class="btn btn-block"><b>Website</b></a></div>
             </div>
         </div>
         <div class="col-sm-4 col-md-4">
-            <div class="pricingTable yellow">
-                <div class="pricingTable-header">
+            <div class="p-3 mb-2 bg-light text-dark">
+                <div class="p-3 mb-2 bg-light text-dark">
                     <img class="rounded-circle" src="{{ asset('images/2.jpg') }}" style="height:110px;width:110px">
                     <h3>BBC</h3><span>BBC Mumbai Desk</span></div>
-                <div class="pricing-plans"><b><span class="priceContent" style="text-size:90px;color:antiquewhite"><span>Crew Members</span></span></b>
+               <h5><div class="p-3 mb-2 bg-light text-dark"><b><span class="priceContent" style="text-size:90px;color:navy"><span>Crew Members</span></span></b>
+                </div></h5>
+                <div class="p-3 mb-2 bg-light text-dark">
+                <div class="p-3 mb-2 bg-light text-dark" style="background:azure">
+                        Vijay<br/>
+                        Abhilash<br/>
+                        Adin<br/>
+                        Jisha<br/>
                 </div>
-                <div class="pricingContent" style="background:azure">
-                    <ul>
-                        <li>Vijay</li>
-                        <li>Abhilash</li>
-                        <li>Adin</li>
-                        <li>Jisha</li>
-                    </ul>
                 </div>
-                <div class="pricingTable-sign-up"><a href="#" class="btn btn-block">website</a></div>
+                <div class="btn btn-danger"><a href="#" class="btn btn-block"><b>Website</b></a></div>
             </div>
         </div>
     </div>
+    </center>
     <!--<div class="footer-basic">
         <footer>
             <p class="copyright">Dept. CSA Amrita University Amritapuri Campus © 2018</p>
