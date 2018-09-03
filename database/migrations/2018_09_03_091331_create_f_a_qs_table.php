f9224162cb550b28aad531883f0f8de9a1fde8af
PS NaraYanan <thepsnarayanan@gmail.com>
2018-09-03 14:46:01 +0530

    FAQ Module

---
+++ database/migrations/2018_09_03_091331_create_f_a_qs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFAQsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('f_a_qs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('f_a_qs');
+    }
+}
