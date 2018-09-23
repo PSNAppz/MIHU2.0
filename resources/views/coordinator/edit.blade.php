841500071bfb607dbf1d6e74a31ea905affab777
PS NaraYanan <thepsnarayanan@gmail.com>
2018-09-23 15:22:32 +0530

    Volunteers patch #1

---
+++ resources/views/coordinator/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.default') @section('content')
+<br>
+<div class="container">
+   @if($errors->any())
+   <div class="alert alert-danger">
+      @foreach($errors->all() as $error)
+      <p>{{ $error }}</p>
+      @endforeach
+   </div>
+   @endif
+   <h1 class="display-4" style="color:white;">Add new volunteer details</h1>
+   <div class="col-sm">
+      {{ Form::model($cord, array('route' => array('coordinator.update', $cord->id),'data-parsley-validate' => '', 'method' => 'PUT')) }}
+      {{ Form::model($cord, array('route' => array('coordinator.update', $cord->id),'data-parsley-validate' => '', 'method' => 'PUT')) }}
+        {{ Form::label('department', 'Department:') }}
+        {{ Form::text('department',$cord->department,array('class'=> 'form-control','required' => ''))}}
+        {{ Form::label('name','Name:')}}
+        {{ Form::text('name',$cord->name,array('class'=> 'form-control','required' => ''))}}
+        {{ Form::label('seva','Seva:')}}
+        {{ Form::text('seva',$cord->seva,array('class'=> 'form-control','required'=> ''))}}
+        {{ Form::label('contact','Contact No:')}}
+        {{ Form::text('contact',$cord->contact,array('class'=> 'form-control','required'=> ''))}}
+      <center>
+         {{ Form::submit('Edit Details',array('class'=>'btn btn-success ','style' =>'margin-top:20px;'))}}
+         <a class="btn btn-danger " href="{{ url('/coordinator') }}" role="button" style="margin-top:20px">Cancel</a>
+      </center>
+      {!! Form::close() !!}
+   </div>
+</div>
+</div>
+@endsection
---
+++ resources/views/coordinator/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.default') @section('content')
+<div class="container">
+   @if(Session::has('success'))
+   <div class="alert alert-info">
+      {{ Session::get('success') }}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+      </button>
+   </div>
+   @endif
+   <script>
+      $(document).ready( function () {
+       $('#cordunteer').DataTable();
+      } );
+   </script>
+   <style type="text/css">
+      body{
+      color:white;
+      }
+   </style>
+   <h2 class="display-4" style="color:white;">Coordinators</h2>
+   <br>
+   <nav>
+      <div class="nav nav-tabs" id="nav-tab" role="tablist">
+         <a class="nav-item nav-link active" id="nav-student-tab" data-toggle="tab" href="#nav-student" role="tab" aria-controls="nav-student" aria-selected="true">Coordinators</a>
+      </div>
+      <br>
+      @if(!Auth::guest())
+      <a class="btn btn-primary " href="{{ url('/coordinator/create') }}" role="button" >Add New +</a>
+      <a  id="xlsf" href="{{ URL::to('downloadExcel/coordinators/xls') }}"><button class="btn btn-info">Download Excel xls</button></a>
+      <a id="xlsxf" href="{{ URL::to('downloadExcel/coordinators/xlsx') }}"><button class="btn btn-info">Download Excel xlsx</button></a>
+      <a id="csvf" href="{{ URL::to('downloadExcel/coordinators/csv') }}"><button class="btn btn-info">Download CSV</button></a>
+      @endif
+      <br>
+      <br>
+   </nav>
+   <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="nav-student" role="tabpanel" aria-labelledby="nav-student-tab">
+         <table id="cordunteer" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%;color:white;">
+            <thead>
+               <tr>
+                  <th>Name</th>
+                  <th>Seva</th>
+                  <th>Department</th>
+                  <th>Contact</th>
+                  @if(!Auth::guest())
+                  <th>Update</th>
+                  <th>Delete</th>
+                  @endif
+               </tr>
+            </thead>
+            <tbody>
+               @foreach($coordinators as $cord)
+               <tr>
+                  <th>{{ $cord->name}}</th>
+                  <th>{{ $cord->seva}}</th>
+                  <th>{{ $cord->department}}</th>
+                  <th>{{$cord->contact}}</th>
+                  @if(!Auth::guest())
+                  <th><a class="btn btn-warning" href="{{ route('coordinator.edit', $cord->id,'/edit') }}" role="button">Update</a></th>
+                  <th>  {{ Form::open(['method' => 'DELETE', 'route' => ['coordinator.destroy', $cord->id]]) }}
+                     {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                     {{ Form::close() }}
+                  </th>
+                  @endif
+               </tr>
+               @endforeach
+            </tbody>
+         </table>
+      </div>
+   </div>
+</div>
+@endsection
