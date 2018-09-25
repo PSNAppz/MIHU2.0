7db284a24d20de6ba9b3a4e982e513b5ce20874a
imarjunv <arjunvijayanathakurup@gmail.com>
2018-09-25 10:12:16 +0530

    added map module

---
+++ resources/views/maps/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default') @section('content')
+
+	<div class="container">
+		<center><img src="{{ asset('images/Map1.jpg') }}" alt=" Map1" width="100%" height="70%"></center>	
+	</div>
+
+@endsection
