44ac76c8750f76238d42258b6dc1cb74981dd6e5
imarjunv <arjunvijayanathakurup@gmail.com>
2018-09-26 13:35:16 +0530

    added info image

--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -3,17 +3,23 @@
 	<div class="container">
       <nav>
 		 <div class="nav nav-tabs" id="nav-tab" role="tablist">
-         <a class="nav-item nav-link active" id="nav-map-tab" data-toggle="tab" href="#nav-map" role="tab" aria-controls="nav-map" aria-selected="true">Toilets</a>
+         <a class="nav-item nav-link active" id="nav-info-tab" data-toggle="tab" href="#nav-info" role="tab" aria-controls="nav-info" aria-selected="true">Toilets</a>
          <a class="nav-item nav-link" id="nav-loc-tab" data-toggle="tab" href="#nav-loc" role="tab" aria-controls="nav-loc" aria-selected="false">Location Info</a>
+         <a class="nav-item nav-link" id="nav-map-tab" data-toggle="tab" href="#nav-map" role="tab" aria-controls="nav-map" aria-selected="false">Location Info</a>
       </div>
    </nav>
    <div class="tab-content" id="nav-tabContent">
-   	<div class="tab-pane fade show active" id="nav-map" role="tabpanel" aria-labelledby="nav-map-tab">
-         <center><img src="{{ asset('images/toi.png') }}" alt=" Map2" width="100%" height="70%"></center>   
+      <div class="tab-pane fade  show active" id="nav-info" role="tabpanel" aria-labelledby="nav-info-tab">
+            <center><img src="{{ asset('images/info1.jpg') }}" alt=" Map2" width="100%" height="70%"></center>   
       </div>
       <div class="tab-pane fade" id="nav-loc" role="tabpanel" aria-labelledby="nav-loc-tab">
-         <center><img src="{{ asset('images/locinfo.png') }}" alt=" Map2" width="100%" height="70%"></center>   
+            <center><img src="{{ asset('images/locinfo.png') }}" alt=" Map2" width="100%" height="70%"></center>   
       </div>
+   	<div class="tab-pane fade" id="nav-map" role="tabpanel" aria-labelledby="nav-map-tab">
+         <center><img src="{{ asset('images/toi.png') }}" alt=" Map2" width="100%" height="70%"></center>   
+      </div>
+     
+      
    </div>
 	</div>
 
